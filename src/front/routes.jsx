6fdc43c9b43a94af421f8378325d1dc67425758c
axelluribe <axelluribe13@gmail.com>
2025-11-19 19:37:01 +0000

    First Commit

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -8,30 +8,17 @@ import Demo from "./pages/Demo";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-
-  if (!token) {
-    return <Login />; 
-  }
-
-  return children;
-};
-
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-
       <Route element={<Layout />}>
         <Route index element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/single/:theId" element={<Single />} />
+        <Route path="/demo" element={<Demo />} />
       </Route>
-
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
-      <Route path="/home" element={<ProtectedRoute> <Home /></ProtectedRoute>}/>
-      <Route path="/single/:theId" element={ <ProtectedRoute> <Single /></ProtectedRoute>} />
-      <Route path="/demo"element={<ProtectedRoute> <Demo /></ProtectedRoute>} />
     </>
   )
 );
